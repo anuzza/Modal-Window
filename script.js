08ca767a4c8cdeec79792440e0b9dd20fb3c0245
anuzza <anuja3580@gmail.com>
2022-01-13 16:48:01 -0600

    corrected spelling of class

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModdal = document.querySelector('.close-model');
+const btnCloseModdal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
